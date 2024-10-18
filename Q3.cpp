fe32ca2cb6c210a8cc4aeccfc170b3ba8fdc9055
Ahmad Raza <ahmedrazawarriach786@gmail.com>
2024-10-18 14:57:55 +0500

    traingle patterns

--- Q3.cpp
+++ Q3.cpp
@@ -2,10 +2,47 @@
 using namespace std;
 int main()
 {
-    int n=10;
+
+  //patter#1
+
+    int n=5;
+    // for(int i=0;i<n;i++){
+    //   for(int j=0;j<=i;j++){
+    //  cout<<"* ";
+    //   }
+    //   cout<<endl;
+    // }
+
+    //patter#2
+
+    // 
+    // for(int i=0;i<n;i++){
+    //   for(int j=0;j<i+1;j++){
+    //     cout<<(i+1);
+
+    //   }
+    //   cout<<endl;
+    // }
+
+    //patter#3
+
+    //     
+    //    char ch='A';
+    // for(int i=0;i<n;i++){
+       
+    //   for(int j=0;j<i+1;j++){
+    //     cout<<ch<<"";
+
+    //   }
+    //   cout<<endl;
+    //   ch++;
+    // }
+
+    //patter#4
+
     for(int i=0;i<n;i++){
-      for(int j=0;j<=i;j++){
-     cout<<"*";
+      for(int j=1;j<i+1;j++){
+        cout<<j;
       }
       cout<<endl;
     }
