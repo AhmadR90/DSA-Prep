a4382f0a8ab4b41ed447fbaccb6da95e73ce3cd0
Ahmad Raza <ahmedrazawarriach786@gmail.com>
2024-10-18 14:26:39 +0500

    initial commit

---
+++ Q2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //for numbers
+
+    
+    // int n=4;
+    // int num=1;
+    // for(int i=0;i<n;i++){
+    //     for(int j=0;j<n;j++){
+    //         cout<<num <<"";
+    //         num++;
+    //     }
+    //     cout<<endl;
+    // }
+
+
+    //for characters
+      int n=4;
+    char ch='A';
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<ch ;
+            ch++;
+        }
+        cout<<endl;
+    }
+}
---
+++ patterns.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 4;
+    for (int i = 0; i <= n - 1; i++)
+    {
+        //char ch = 'A';
+        for (int j = 1; j <= n; j++)
+        { // line start
+
+
+
+            // // for characters
+            // cout << ch;
+            // ch = ch + 1;
+
+            // // for numbers
+            // // cout<<j;
+
+            // // for stars
+            // //  cout<<"*";
+        }
+        cout << endl;
+    }
+}
